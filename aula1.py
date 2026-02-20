cb2d198d55a02a2408f7ac46622efe3397b15388
Hyllari <isadora.barbosa29@escola.pr.gov.br>
2026-02-20 08:03:54 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,11 @@
+print("Oieee")
+nome = "Hyllari"
+idade = 16
+altura = 1.64
+#Usar o print com variáveis:
+print("Oiiee meu nome é", nome, "eu tenho", idade, "anos", "minha altura é", altura)
+
+#F- Strings
+print(f"Oiiee, meu nome é {nome}, eu tenho {idade} anos, e tenho {altura} de altura")
+
+#Para executar no terminal: python3 nomedoarquivo.py
